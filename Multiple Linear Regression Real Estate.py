ac69a06a05f5718d93e78f731f1c8c233cc04b64
ku18 <107715385+ku18@users.noreply.github.com>
2025-08-03 22:39:42 +0530

    Update Multiple Linear Regression Real Estate.py

--- Multiple Linear Regression Real Estate.py
+++ Multiple Linear Regression Real Estate.py
@@ -1,82 +1,26 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Multiple Linear Regression - Exercise
-
-# You are given a real estate dataset. 
-# 
-# Real estate is one of those examples that every regression course goes through as it is extremely easy to understand and there is a (almost always) certain causal relationship to be found.
-# 
-# The data is located in the file: 'real_estate_price_size_year.csv'. 
-# 
-# You are expected to create a multiple linear regression (similar to the one in the lecture), using the new data. 
-# 
-# In this exercise, the dependent variable is 'price', while the independent variables are 'size' and 'year'.
-# 
-# Good luck!
-
-# ## Import the relevant libraries
-
-# In[1]:
-
+# Real Estate Price Prediction using Multiple Linear Regression
+# Author: Kushagra Goyal
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
-import seaborn
-seaborn.set()
+import seaborn as sns
 
+sns.set()
 
-# ## Load the data
-
-# In[2]:
-
-
+# Load dataset
 data = pd.read_csv("real estate price size year.csv")
 
-
-# In[3]:
-
-
-data
-
-
-# In[4]:
-
-
-data.describe
-
-
-# ## Create the regression
-
-# ### Declare the dependent and the independent variables
-
-# In[8]:
-
-
+# Define features and target variable
+X = data[['size', 'year']]
 y = data['price']
-x1 = data[['size','year']]
 
+# Add a constant term to the model
+X = sm.add_constant(X)
 
-# ### Regression
+# Fit the regression model
+model = sm.OLS(y, X).fit()
 
-# In[9]:
-
-
-x = sm.add_constant(x1)
-results = sm.OLS(y,x).fit()
-
-
-# In[10]:
-
-
-#find summary
-results.summary()
-
-
-# In[ ]:
-
-
-
-
+# Display model summary
+print(model.summary())
